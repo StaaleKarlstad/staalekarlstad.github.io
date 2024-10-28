80792b582c6ae69f9b97bca246a13bc6cc372a61
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-10-28 18:11:43 +0100

    Update project descriptions

--- src/data.tsx
+++ src/data.tsx
@@ -1,37 +1,52 @@
-export const projects = [{
-    title: "Buncharted",
-    subtitle: "Java",
+export const projects = [
+  {
+    title: "Graylien",
+    subtitle: "React TS, Java Spring Boot, PostgreSQL, Docker",
     description:
-      "Rouge like mini-game. Group project for school.",
+      "A practice tool designed for artists looking to enhance their skills in grayscale drawing and painting. This project is my first solo full-stack endeavor, through which I've gained hands-on experience in writing a REST API, PostgreSQL databases, and containerization. Currently in development",
+    image: "/Graylien.png",
+    link: "https://github.com/StaaleKarlstad/Graylien",
+  },
+  {
+    title: "Buncharted",
+    subtitle: "Java, Logic x",
+    description: "In this wild roguelike shooter, you play as an armed robot chef tasked with eradicating evil pastries in the cake dimension! Are you the right robot for the task? This game was a collaborative school project with a team of four. Along with programming, I had the opportunity to compose the music.",
     image: "/buncharted_screenshot_01.png",
     link: "https://git.app.uib.no/microissant/project-gluten",
   },
   {
     title: "Handl",
-    subtitle: "React, C#, .NET, PostGres",
+    subtitle: "React, C#, .NET, PostgreSQL, Docker, Postman",
     description:
-      "Application for creating and managing shopping lists. Currently in development",
+      "Handl is a web application designed for creating and managing shopping lists. Developed in collaboration with a fellow computer science student, this project introduced us to industry-relevant software practices and architectures, such as REST APIs. Handl is currently in development.",
     image: "/handl.png",
     link: "",
   },
   {
-    title: "Personal website",
-    subtitle: "React TS and Tailwind CSS",
+    title: "Little Lemon",
+    subtitle: "React JS, CSS, HTML, Figma",
     description:
-    "The website you are currently on",
-    image: "/personal_website_img.png",
+      "The Little Lemon website is a fully responsive, user-friendly web application that allows customers to explore the restaurant menu, make reservations, and learn more about this fictional restaurant. The application uses modern React development practices, including components, state management, and routing. It is the capstone project for Meta's Front-End Developer Certification.",
+    image: "/LittleLemon.png",
+    link: "https://github.com/StaaleKarlstad/LittleLemon",
+  },
+  {
+    title: "Personal website",
+    subtitle: "React TS, Tailwind CSS",
+    description:
+    "This is the website you are currently visiting. It was my first big solo project and I had a lot of fun exploring React and Tailwind. Hosting is done using github pages",
+    image: "/personal_website2.png",
     link: "https://github.com/StaaleKarlstad/personal-website",
   },
   {
     title: "Catch exploration",
-    subtitle: "Python and SciKit-learn",
-    description:
-    "Pre prossessing of a data set and training different machine learning models. School project",
+    subtitle: "Python, SciKit-learn, Jupyter Notebook",
+    description: "This school project offered hands-on experience with training and evaluating machine learning models. Using data from Norwegian fishing boat records, we aimed to predict the expected fish catch weight for each operation. This was a collaborative project with another student",
     image: "/catch_machine_learning.png",
-    link: "https://github.com/StaaleKarlstad/INFO284",
+    link: "https://github.com/StaaleKarlstad/INFO284/blob/develop/Notebooks/GroupExamINFO284.ipynb",
   },]
 
-export const skills = ["Python", "Java", "Typescript", "React", "Tailwind CSS", "SciKit-learn"]
+export const skills = ["Java Spring Boot","React/Typescript", "Python", "CSS", "HTML", "Docker", "SciKit-learn", "Figma", "Postman"]
 
 export const drawings = ["/adventurer_web.jpg", 
                         "/debug_earth_web.jpg", 
