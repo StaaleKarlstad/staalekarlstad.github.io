272cd70b2134e4fe4a9e34542005df896eff35ff
StaaleKarlstad <107145721+StaaleKarlstad@users.noreply.github.com>
2024-10-31 10:56:38 +0100

    Change project order

--- src/data.tsx
+++ src/data.tsx
@@ -1,32 +1,16 @@
 export const projects = [
   {
-    title: "- -- - -- Graylien -- - -- -",
-    subtitle: "React TS, Java Spring Boot, PostgreSQL, Docker",
-    description:
-      "A practice tool designed for artists looking to enhance their skills in grayscale drawing and painting. This project is my first solo full-stack endeavor, through which I've gained hands-on experience in writing a REST API, PostgreSQL databases, and containerization. Currently in development",
-    image: "./Graylien.png",
-    link: "https://github.com/StaaleKarlstad/Graylien",
-  },
-  {
     title: "- -- - -- Buncharted -- - -- -",
     subtitle: "Java, Logic x",
-    description: "In this wild roguelike shooter, you play as an armed robot chef tasked with eradicating evil pastries in the cake dimension! Are you the right robot for the task? This game was a collaborative school project with a team of four. Along with programming, I had the opportunity to do sound design and compose the music.",
+    description: "In this wild roguelike shooter, you play as an armed robot chef tasked with eradicating evil pastries in the cake dimension! Are you the right robot for the task? This game was a collaborative school project with a team of four. Along with programming, I had the opportunity to do sound design and compose the music. Scroll to the bottom of the page to check out a snippet.",
     image: "./buncharted_screenshot_01.png",
     link: "https://git.app.uib.no/microissant/project-gluten",
-  },
-  {
-    title: "- -- - -- Handl -- - -- -",
-    subtitle: "React, C#, .NET, PostgreSQL, Docker, Postman",
-    description:
-      "Handl is a web application designed for creating and managing shopping lists. Developed in collaboration with a fellow computer science student, this project introduced us to industry-relevant software practices and architectures, such as REST APIs. Handl is currently in development.",
-    image: "./handl.png",
-    link: "",
   },
   {
     title: "- -- - -- Little Lemon -- - -- -",
     subtitle: "React JS, CSS, HTML, Figma",
     description:
-      "The Little Lemon website is a fully responsive, user-friendly web application that allows customers to explore the restaurant menu, make reservations, and learn more about this fictional restaurant. The application uses modern React development practices, including components, state management, and routing. It is the capstone project for Meta's Front-End Developer Certification.",
+    "The Little Lemon website is a fully responsive, user-friendly web application that allows customers to explore the restaurant menu, make reservations, and learn more about this fictional restaurant. The application uses modern React development practices, including components, state management, and routing. It is the capstone project for Meta's Front-End Developer Certification.",
     image: "./LittleLemon.png",
     link: "https://github.com/StaaleKarlstad/LittleLemon",
   },
@@ -37,6 +21,22 @@ export const projects = [
     "This is the website you are currently visiting. It was my first big solo project and I had a lot of fun exploring React and Tailwind. Hosting is done using github pages",
     image: "./personal_website2.png",
     link: "https://github.com/StaaleKarlstad/personal-website",
+  },
+  {
+    title: "- -- - -- Graylien -- - -- -",
+    subtitle: "React TS, Java Spring Boot, PostgreSQL, Docker",
+    description:
+      "A practice tool designed for artists looking to enhance their skills in grayscale drawing and painting. This project is my first solo full-stack endeavor, through which I've gained hands-on experience in writing a REST API, PostgreSQL databases, and containerization. Currently in development.",
+    image: "./Graylien.png",
+    link: "https://github.com/StaaleKarlstad/Graylien",
+  },
+  {
+    title: "- -- - -- Handl -- - -- -",
+    subtitle: "React, C#, .NET, PostgreSQL, Docker, Postman",
+    description:
+    "Handl is a web application designed for creating and managing shopping lists. Developed in collaboration with a fellow computer science student, this project introduced us to industry-relevant software practices and architectures, such as REST APIs. Handl is currently in development.",
+    image: "./handl.png",
+    link: "",
   },
   {
     title: "-- - -- Catch exploration -- - --",
